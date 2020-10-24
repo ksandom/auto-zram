1d50054f00385b5cfca1679d7deb5f32dcb38dea
Kevin Sandom <kjsandom@gmail.com>
2020-10-24 19:12:59 +0100

    Make the compression ration configurable

--- config/auto-zram.sh
+++ config/auto-zram.sh
@@ -15,3 +15,6 @@ coresToKeepFree=1
 
 # Swappiness. There are lots of opinions about what this should be set to. I suggest googling it and working out what is right for you and your workload. Here's a promising article: https://www.howtogeek.com/449691/what-is-swapiness-on-linux-and-how-to-change-it/
 swappiness=0
+
+# compressionRatio. How many times bigger our compressed swap is going to be than the amount of RAM we are using to feed it. Eg If we set this to 3.2, it would mean that we are telling the ZRAM module that our data can easily compress to the degree that we could fit 3.2 times as much data into the same place. The ZRAM module will trust you, and horrible/hard to debug things will happen some time in the future if you are wrong. Setting this number lower is safer. Setting it higher gives you more useable swap. 2 seems to be fairly safe, in that I have not been able to crash it with any of my workloads yet. If you set this too high and things crash. Don't panic. All you need to do is set it to a lower value and reboot. Do understand that in this situation, and data that was being worked with when the ZRAM module ran out of memory, is likely corrupted. So anything that was being worked on should not be trusted.
+compressionRatio=2
